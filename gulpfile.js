6d322d67e0d532746046fa94cab9c634404613ef
shuoshubao <759979885@qq.com>
2016-02-29 13:10:05 +0800

    去掉seajs 并加入了gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var transport = require("gulp-seajs-transport");
-
-// transport - seajs
-gulp.task("transport",function(){
-  gulp.src('sta/js-dist/*.js')
-    .pipe(transport())
-    .pipe(gulp.dest('sta/js/'))
-});
 
 // 压缩js
 gulp.task('min-js', function () {
-  gulp.src('sta/js-dist/*.js')
+  gulp.src('sta/js/src/*.js')
       .pipe(uglify())
-      .pipe(gulp.dest('sta/js/'))
+      .pipe(gulp.dest('sta/js/dest/'))
 });
 
 //
