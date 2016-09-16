c9e6192b5395458d910191f44592d04041688ea7
shuoshubao <759979885@qq.com>
2016-09-16 22:34:26 +0800

    remove electron

--- server.js
+++ server.js
@@ -4,36 +4,21 @@ var server = new http.Server();
 
 server.listen(8000);
 
-server.on('request', function(request, response) { console.log('request');
+server.on('request', function(request, response) {
   var url = require('url').parse(request.url);
   switch (url.pathname) {
     case '' || '/':
       fs.readFile('./index.html', function(err, content) {
-        if (err) { response.writeHead(404, { 'Content-Type': 'text/plain; charset="UTF-8"' });
+        if (err) {
+          response.writeHead(404, { 'Content-Type': 'text/plain; charset="UTF-8"' });
           response.write(err.message);
-          response.end() } else { response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+          response.end()
+        } else {
+          response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
           response.write(content);
-          response.end() } });
-      break;
-    case '/test/delay':
-      var delay = parseInt(url.query) || 2000;
-      response.writeHead(200, { 'Content-type': 'text/plain; charset=UTF-8' });
-      response.write('Sleeping for' + delay + ' milliseconds...');
-      setTimeout(function() { response.write('done.');
-        response.end() }, delay);
-      break;
-    case '/test/mirror':
-      response.writeHead(200, { 'Content-type': 'text/plain; charset=UTF-8' });
-      response.write(request.mothod + ' ' + request.url + ' HTTP/' + request.httpVersion + '\r\n');
-      for (var h in request.headers) { response.write(h + ':' + request.headers[h] + '\r\n') }
-      response.write('\r\n');
-      request.on('data', function(chunk) { response.write(chunk) });
-      request.on('end', function(chunk) { response.end() });
-      break;
-    case '/json':
-      response.writeHead(200, { 'Content-type': 'application/json; charset=UTF-8' });
-      response.write(JSON.stringify({ test: 'success' }));
-      response.end();
+          response.end()
+        }
+      });
       break;
     default:
       var filename = url.pathname.substring(1);
@@ -43,7 +28,7 @@ server.on('request', function(request, response) { console.log('request');
           response.writeHead(404, { 'Content-Type': 'text/plain; charset="UTF-8"' });
           response.write(err.message);
           response.end()
-        }else {
+        } else {
           response.writeHead(200, { 'Content-Type': type });
           response.write(content);
           response.end()
@@ -57,20 +42,10 @@ function getType(endTag) {
   var type = null;
   switch (endTag) {
     case 'html':
-    case 'htm':
       type = 'text/html; charset=UTF-8';
       break;
     case 'js':
       type = 'application/javascript; charset="UTF-8"';
-      break;
-    case 'css':
-      type = 'text/css; charset="UTF-8"';
-      break;
-    case 'txt':
-      type = 'text/plain; charset="UTF-8"';
-      break;
-    case 'manifest':
-      type = 'text/cache-manifest; charset="UTF-8"';
       break;
     default:
       type = 'application/octet-stream';
