ad8114fd9144f1b7a1a607bcf4e801aaf8e77621
shuoshubao <759979885@qq.com>
2017-03-03 14:02:57 +0800

    🔨 eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "classes": true,
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "react/require-default-props": [
+          "error",
+          "never"
+        ]
+    }
+};
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 * http://localhost:8080
 * npm start 编译并监听 react
 * npm run build 编译并压缩 react
+* npm run eslint
