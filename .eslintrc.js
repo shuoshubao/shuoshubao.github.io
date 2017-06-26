3570681b535b11562308e3b9e846f55e86860b52
shuoshubao <759979885@qq.com>
2017-06-26 15:15:26 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "extends": "airbnb",
+  "extends": [
+    "eslint:recommended",
+    "airbnb"
+  ],
   "parser": "babel-eslint",
   "plugins": [
 
@@ -19,17 +22,32 @@ module.exports = {
     "eqeqeq": [2, "always"],
     // [] {} 前后空格
     "object-curly-spacing": [2, "never"],
+    // 左大括号
+    "space-before-blocks": [2],
     // const 被改变
     "no-const-assign": [2],
     // const let 没重新赋值的用const
-    "prefer-const": [2, {
+    "prefer-const": [1, {
       "destructuring": "any",
       "ignoreReadBeforeAssign": false
     }],
     // 变量声明了没被使用
-    "no-unused-vars": [2],
+    // "no-unused-vars": [2],
+    // 变量声明覆盖
+    "no-shadow": [2, {
+      "builtinGlobals": false,
+      "hoist": "functions",
+      "allow": []
+    }],
     // 操作符前后加空格
     "space-infix-ops": [2],
+    // 逗号前后的空格
+    "comma-spacing": [2, {
+      "before": false,
+      "after": true
+    }],
+    // 单行块内 {} 空格
+    "block-spacing": [2, "never"],
     // 关键字前后空格
     // as、async、await、break、case、catch、class、const、continue、debugger、default、delete、do、else、export、extends、finally、for、from、function、get、if、import、in、instanceof、let、new、of、return、set、static、super、switch、this、throw、try、typeof、var、void、while、with、yield
     "keyword-spacing": [2, {
@@ -42,14 +60,28 @@ module.exports = {
         "while": {"after": false}
       }
     }],
+    // parseInt 基数
+    // "radix": [2, "always"],
+    // 禁止在返回语句中赋值
+    // "no-return-assign": [2, "except-parens"],
+    // () 前后空格
+    "space-in-parens": [2],
     // 一行的最大长度, 默认80
     "max-len": [0],
     // 最多连续多少空行
-    "no-multiple-empty-lines": [2, { "max": 2 }],
+    "no-multiple-empty-lines": [2, {
+      "max": 2
+    }],
+    // 以空行开始并且以空行结束
+    "padded-blocks": [2],
     // 一个表达式含有多个运算符
     "no-mixed-operators": [0],
     // 箭头函数的返回值是三目的时候, 用括号包起来
-    "no-confusing-arrow": [2, {"allowParens": true}],
+    "no-confusing-arrow": [2, {
+      "allowParens": true
+    }],
+    // 没必要的escape \
+    "no-useless-escape": [2],
     // react 组件method顺序
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
     "react/sort-comp": [1],
@@ -63,10 +95,10 @@ module.exports = {
     "react/jsx-indent": [2, 2],
     // react ref属性值不能是字符串 必须是函数
     "react/no-string-refs": [2],
-    // react prop必须有类型验证
-    "react/prop-types": [2],
+    // react prop需要有类型验证
+    "react/prop-types": [1],
     // react array 的key值不要使用数组的索引, 或包含索引的表达式
-    // "react-no-array-index-key": [2],
+    "react/no-array-index-key": [1],
     // react jsx role
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-static-element-interactions.md
     "jsx-a11y/no-static-element-interactions": [0],
@@ -76,12 +108,45 @@ module.exports = {
     "arrow-body-style": [2, "as-needed"],
     // 当有多行jsx时, 使用() + 换行
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-wrap-multilines.md
-    "react/jsx-wrap-multilines": [2, {"assignment": true}],
+    "react/jsx-wrap-multilines": [2, {
+      "assignment": true
+    }],
+    // react jsx闭括号换行
+    // "react/jsx-closing-bracket-location": [2, "tag-aligned"],
+    // react jsx 每行最多属性数量
+    "react/jsx-max-props-per-line": [2, {
+      "maximum": 2
+    }],
+    //
+    "react/jsx-no-bind": [2, {
+      "allowArrowFunctions": true,
+      "ignoreRefs": true,
+      "allowBind": true
+    }],
     // 为函数指定返回值
     "consistent-return": [0],
     // class的methods中用this
     "class-methods-use-this": [1],
+    // import 绝对路径在相对路径之前
+    "import/first": [0],
     // import 相对路径
     "import/no-unresolved": [0],
+    // import package.json dependencies
+    "import/no-extraneous-dependencies": [0],
+    // import 文件后缀
+    "import/extensions": [2, {
+      ".js": "never",
+      ".jsx": "never"
+    }],
+    // <img> 的 alt
+    "jsx-a11y/alt-text": [0],
+    // JSX 属性双引号
+    // "jsx-quotes": [2, "prefer-double"],
+    // 对象字面量简写
+    "object-shorthand": [2, "always"],
+    // 函数参数被重新赋值
+    /*"no-param-reassign": [2, {
+      "props": false
+    }],*/
   }
 }
