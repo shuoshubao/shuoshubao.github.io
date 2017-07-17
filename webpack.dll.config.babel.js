bf43dffd52f8aa6fe09e7c3126138b1f62d0e078
shuoshubao <759979885@qq.com>
2017-07-17 16:20:10 +0800

    dll 压缩

--- webpack.dll.config.babel.js
+++ webpack.dll.config.babel.js
@@ -1,21 +1,25 @@
 import fs from 'fs'
 import path from 'path'
 import webpack from 'webpack'
+import {exec} from 'child_process'
+
+exec('rm src/lib/*')
 
 module.exports = {
-　　entry: {
-　　　　vendor: ['react', 'react-dom', 'prop-types', 'classnames']
-　　},
-　　output: {
-　　　　path: path.join(__dirname, 'src/lib'),
-　　　　filename: '[name].js',
-　　　　library: '[name]'
-　　},
-　　plugins: [
-　　　　new webpack.DllPlugin({
-　　　　　　context: __dirname,
-　　　　　　path: path.join(__dirname, 'src/lib', 'manifest.json'),
-　　　　　　name: '[name]'
-　　　　})
-　　]
+  entry: {
+    vendor: ['react', 'react-dom', 'prop-types', 'classnames']
+  },
+  output: {
+    path: path.join(__dirname, 'src/lib'),
+    filename: '[name].js',
+    library: '[name]'
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      context: __dirname,
+      path: path.join(__dirname, 'src/lib', 'manifest.json'),
+      name: '[name]'
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 }
