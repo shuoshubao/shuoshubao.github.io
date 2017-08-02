216222238fb4a4d70bcd676802d2641e9f001137
shuoshubao <759979885@qq.com>
2017-08-02 12:26:14 +0800

    deploy view

--- bin/qiniu.js
+++ bin/qiniu.js
@@ -4,8 +4,6 @@ import qiniu from 'qiniu'
 import chalk from 'chalk'
 import glob from 'glob'
 
-const fileList = glob.sync('build/*').map(v => v.slice(6))
-
 qiniu.conf.ACCESS_KEY = 'LKMVIJ4M-zOvdOMOFLoohglxHuOkJz21IALeEcMB'
 qiniu.conf.SECRET_KEY = 'ijkKQkeGIvF35rrua-dRtRDd0Uc2Zpwj1jAzP8pZ'
 const bucket = 'shuoshubao'
@@ -15,15 +13,24 @@ const uploadFile = (uptoken, key, localFile) => {
   return new Promise((resolve, reject) => qiniu.io.putFile(uptoken, key, localFile, extra, (err, ret) => err ? reject(err) : resolve()))
 }
 
-Promise.all(fileList.map(v => {
-  console.log(chalk.cyan(`正在上传: ${v}`))
-  return uploadFile(uptoken(bucket, v), v, path.join(__dirname, `../build/${v}`))
-}))
-.then(() => {
-  console.log(chalk.green('上传成功'))
-  console.log(chalk.green(`共上传${fileList.length}个文件`))
-})
-.catch(e => {
-  console.log(chalk.red('上传失败'))
-  console.error(e)
-})
+const deployToQiniu = (fileList, root) => {
+    Promise.all(fileList.map(v => {
+      console.log(chalk.cyan(`正在上传: ${v}`))
+      return uploadFile(uptoken(bucket, v), v, path.join(__dirname, `../${root}/${v}`))
+    }))
+    .then(() => {
+      console.log(chalk.green('上传成功'))
+      console.log(chalk.green(`共上传${fileList.length}个文件`))
+    })
+    .catch(e => {
+      console.log(chalk.red('上传失败'))
+      console.error(e)
+    })
+}
+
+const fileList = glob.sync('build/*').map(v => v.slice(6))
+
+deployToQiniu(fileList, 'build')
+
+export default deployToQiniu
+
