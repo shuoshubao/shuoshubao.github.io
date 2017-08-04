c2b0b76a9e11f8451edd2b239bd44d1b04542a53
shuoshubao <759979885@qq.com>
2017-08-04 14:55:34 +0800

    hljs add json

--- src/util/highlight.js/lib/index.js
+++ src/util/highlight.js/lib/index.js
@@ -4,6 +4,7 @@ hljs.registerLanguage('xml', require('./languages/xml'));
 hljs.registerLanguage('css', require('./languages/css'));
 hljs.registerLanguage('markdown', require('./languages/markdown'));
 hljs.registerLanguage('javascript', require('./languages/javascript'));
+hljs.registerLanguage('json', require('./languages/json'));
 hljs.registerLanguage('php', require('./languages/php'));
 
 module.exports = hljs;
