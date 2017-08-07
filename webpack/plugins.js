ff93b94d77d6d20d84e53cb344dd7c42a104be35
shuoshubao <759979885@qq.com>
2017-08-07 18:56:25 +0800

    progress

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,5 +1,5 @@
-import os from 'os'
 import path from 'path'
+import os from 'os'
 import webpack from 'webpack'
 import HappyPack from 'happypack'
 import PrepackWebpackPlugin from 'prepack-webpack-plugin'
