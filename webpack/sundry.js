5653933954e13ddb50c8dc5bf23f379de4fc19f5
shuoshubao <759979885@qq.com>
2017-08-20 01:00:45 +0800

    webpack config 杂项 sundry.js

---
+++ webpack/sundry.js
@@ -0,0 +1,20 @@
+import {isDev, port, FILENAME, PATH_PUBLIC} from './config'
+
+export default isDev ? {
+  externals: {},
+  devServer: {
+    port,
+    quiet: true,
+    filename: FILENAME,
+    publicPath: PATH_PUBLIC,
+    stats: {},
+    historyApiFallback: false,
+    proxy: {}
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 1e2,
+    maxAssetSize: 1e6,
+    assetFilter: assetFileName => !(/\.map$/.test(assetFileName))
+  }
+} : {}
--- webpack/webpack.config.babel.js
+++ webpack/webpack.config.babel.js
@@ -2,33 +2,12 @@ import entry from './entry'
 import plugins from './plugins'
 import module from './module'
 import resolve from './resolve'
-import {isDev, PATH_PUBLIC, FILENAME, port} from './config'
+import sundry from './sundry'
 
-const webpackConfig = {
+export default {
   ...entry,
   module,
   plugins,
   resolve,
-  externals: {}
+  ...sundry
 }
-
-if(isDev) {
-  webpackConfig.devtool = 'source-map'
-  webpackConfig.devServer = {
-    port,
-    quiet: true,
-    filename: FILENAME,
-    publicPath: PATH_PUBLIC,
-    stats: {},
-    historyApiFallback: false,
-    proxy: {}
-  },
-  webpackConfig.performance = {
-    hints: false,
-    maxEntrypointSize: 1e2,
-    maxAssetSize: 1e6,
-    assetFilter: assetFilename => !(/\.map$/.test(assetFilename))
-  }
-}
-
-export default webpackConfig
