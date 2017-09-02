3c4c3633b4e5d16c617756840c7de632ea32567d
shuoshubao <759979885@qq.com>
2017-09-03 01:36:48 +0800

    七牛api变更

--- bin/qiniu.js
+++ bin/qiniu.js
@@ -1,29 +1,42 @@
 import fs from 'fs'
 import path from 'path'
-import qiniu from 'qiniu'
-import chalk from 'chalk'
 import glob from 'glob'
+import chalk from 'chalk'
+import qiniu from 'qiniu'
 
-qiniu.conf.ACCESS_KEY = 'LKMVIJ4M-zOvdOMOFLoohglxHuOkJz21IALeEcMB'
-qiniu.conf.SECRET_KEY = 'ijkKQkeGIvF35rrua-dRtRDd0Uc2Zpwj1jAzP8pZ'
+const accessKey = 'LKMVIJ4M-zOvdOMOFLoohglxHuOkJz21IALeEcMB'
+const secretKey = 'ijkKQkeGIvF35rrua-dRtRDd0Uc2Zpwj1jAzP8pZ'
 const bucket = 'shuoshubao'
-const uptoken = (bucket, key) => (new qiniu.rs.PutPolicy(`${bucket}:${key}`)).token()
-const uploadFile = (uptoken, key, localFile) => {
-  let extra = new qiniu.io.PutExtra()
-  return new Promise((resolve, reject) => qiniu.io.putFile(uptoken, key, localFile, extra, (err, ret) => err ? reject(err) : resolve()))
+
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const config = new qiniu.conf.Config()
+const formUploader = new qiniu.form_up.FormUploader(config)
+const putExtra = new qiniu.form_up.PutExtra()
+const options = {
+  scope: bucket
 }
+const putPolicy = new qiniu.rs.PutPolicy(options)
+const uploadToken=putPolicy.uploadToken(mac)
 
-const fileList = glob.sync('build/*').map(v => v.slice(6))
+const log = (str, color) => console.log(color ? chalk[color](str) : str)
 
-Promise.all(fileList.map(v => {
-  console.log(chalk.cyan(`正在上传: ${v}`))
-  return uploadFile(uptoken(bucket, v), v, path.join(__dirname, `../build/${v}`))
-}))
-.then(() => {
-  console.log(chalk.green('上传成功'))
-  console.log(chalk.green(`共上传${fileList.length}个文件`))
+const uploadFile = localFile => new Promise((resolve, reject) => {
+  const key = localFile.split('/').slice(-1)[0]
+  formUploader.putFile(uploadToken, key, localFile, putExtra, (err) => {
+    if(err) {
+      reject(err)
+    }else {
+      log(`成功上传: ${path.relative(process.cwd(), localFile)}`, 'cyan')
+      resolve()
+    }
+  })
 })
+
+const fileList = glob.sync(path.resolve(__dirname, '../build/*'), {nodir: false}).filter(v => !v.endsWith('.html'))
+
+Promise.all(fileList.map(v => uploadFile(v)))
+.then(() => log(`上传成功, 共上传${fileList.length}个文件`, 'green'))
 .catch(e => {
-  console.log(chalk.red('上传失败'))
-  console.error(e)
+  log('上传失败', 'red')
+  log(e, 'red')
 })
