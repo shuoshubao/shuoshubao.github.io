907acbd5a498e7fca50a1ee41776553a2a5c7ec1
shuoshubao <759979885@qq.com>
2017-09-05 11:53:12 +0800

    process.cwd()

--- webpack/config.js
+++ webpack/config.js
@@ -6,7 +6,7 @@ export const port = 8080
 export const isDev = process.env.NODE_ENV === 'development'
 export const FILENAME = `[name]${isDev ? '' : '.[chunkhash:5]'}.js`
 
-export const PATH_ROOT = path.resolve(__dirname, '..')
+export const PATH_ROOT = process.cwd()
 export const PATH_SRC = path.resolve(PATH_ROOT, 'src')
 export const PATH_ASSET = path.resolve(PATH_ROOT, '.temp/asset')
 export const PATH_BUILD = path.resolve(PATH_ROOT, 'build')
