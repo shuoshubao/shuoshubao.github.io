cd8794a3254122b0e097855af249b3092f8a1c09
shuoshubao <759979885@qq.com>
2017-09-14 18:00:52 +0800

    /

--- webpack/entry.js
+++ webpack/entry.js
@@ -4,6 +4,6 @@ import {pathConfig} from './config'
 
 export default glob.sync(`${pathConfig.view}/**/index.js`)
 .reduce((prev, cur) => {
-  prev[relative(pathConfig.view, cur).split('/').slice(0, -1)] = cur
+  prev[relative(pathConfig.view, cur).split('/').slice(0, -1).join('/')] = cur
   return prev
 }, {})
