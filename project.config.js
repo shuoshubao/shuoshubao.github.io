036ec3233457f2edc656ff9d2b12ca0eea2e5e55
fangtao04 <fangtao04@meituan.com>
2019-06-22 01:59:38 +0800

    整理下npm包

--- project.config.js
+++ project.config.js
@@ -9,24 +9,24 @@ module.exports = {
         script: [
             'https://unpkg.com/babel-polyfill@6.26.0/dist/polyfill.min.js',
             isDevelopment ? 'https://unpkg.com/vue@2.6.10/dist/vue.js' : 'https://unpkg.com/vue@2.6.10/dist/vue.min.js',
-            'https://unpkg.com/element-ui@2.8.2/lib/index.js',
+            'https://unpkg.com/element-ui@2.8.2/lib/index.js'
         ],
-        style: ['https://unpkg.com/element-ui@2.8.2/lib/theme-chalk/index.css'],
+        style: ['https://unpkg.com/element-ui@2.8.2/lib/theme-chalk/index.css']
     },
     externals: {
         vue: 'Vue',
-        'element-ui': 'ELEMENT',
+        'element-ui': 'ELEMENT'
     },
     pathConfig: {
         global: 'src/common.js',
-        build: 'dist',
+        build: 'dist'
     },
     devServer: {
         /* proxy: {
             '/api': 'http://localhost:9000/'
         }, */
         headers: {
-            Author: 'shuoshubao',
-        },
-    },
+            Author: 'shuoshubao'
+        }
+    }
 };
