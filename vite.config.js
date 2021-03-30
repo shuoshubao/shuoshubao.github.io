7d44d52461f74984399737d19027067087deb906
fangt11 <fangt11@ke.com>
2021-03-31 01:22:10 +0800

    feat: vite base

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,12 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 
-console.log(111, __dirname, import.meta);
-
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
-    console.log(222, command, mode);
     const isDevelopment = mode === 'development';
     return {
         root: isDevelopment ? __dirname : resolve(__dirname, 'docs'),
-        base: isDevelopment ? '/' : 'https://foo.com/',
+        base: isDevelopment ? '/' : 'https://shuoshubao.github.io/dist/',
         resolve: {
             alias: {
                 '@': resolve(__dirname, 'src')
