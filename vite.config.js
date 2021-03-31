e9324c22a112bc55becccc485385ca0e5edac724
fangt11 <fangt11@ke.com>
2021-03-31 14:15:44 +0800

    ci: build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 
 // https://vitejs.dev/config/
-export default ({ command, mode }) => {
+export default ({ mode }) => {
     const isDevelopment = mode === 'development';
     return {
         base: isDevelopment ? '/' : 'https://shuoshubao.github.io/',
         build: {
-            manifest: true
+            manifest: true,
+            polyfillDynamicImport: false
         },
         resolve: {
             alias: {
