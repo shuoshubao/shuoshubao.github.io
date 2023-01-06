245d23cd786861330c9e21519963aec907dec597
shuoshubao <759979885@qq.com>
2023-01-06 22:07:35 +0800

    ci(vite): code splitting

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default () => {
+export default ({ mode }) => {
+  const isDevelopment = mode === 'development'
   return {
     build: {
+      outDir: 'docs',
+      target: 'chrome100',
       manifest: true,
-      outDir: 'docs'
+      minify: isDevelopment ? false : 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            markdown: ['markdown-it', 'markdown-it-anchor', 'markdown-it-attrs', 'markdown-it-task-lists'],
+            mermaid: ['mermaid'],
+            katex: ['katex']
+          }
+        }
+      }
     },
     resolve: {
       alias: {
