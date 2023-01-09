0da9ce63f346a4f355b54e0b7cf432b9a1e797fa
shuoshubao <759979885@qq.com>
2023-01-09 21:28:12 +0800

    ci: vite 拆包

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,22 @@ export default ({ mode }) => {
         output: {
           manualChunks: {
             react: ['react', 'react-dom'],
+            antd: ['antd'],
+            highlight: [
+              'highlight.js/lib/core',
+              'highlight.js/lib/languages/javascript',
+              'highlight.js/lib/languages/typescript',
+              'highlight.js/lib/languages/xml',
+              'highlight.js/lib/languages/css',
+              'highlight.js/lib/languages/scss',
+              'highlight.js/lib/languages/less',
+              'highlight.js/lib/languages/json',
+              'highlight.js/lib/languages/markdown',
+              'highlight.js/lib/languages/plaintext',
+              'highlight.js/lib/languages/shell',
+              'highlight.js/lib/languages/bash',
+              'highlight.js/lib/languages/php'
+            ],
             markdown: ['markdown-it', 'markdown-it-anchor', 'markdown-it-attrs', 'markdown-it-task-lists'],
             mermaid: ['mermaid'],
             katex: ['katex']
