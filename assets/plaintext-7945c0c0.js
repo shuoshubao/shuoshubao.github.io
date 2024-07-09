3f9f0cf553eeaa3b838d4d16421097b1328ed6aa
shuoshubao <shuoshubao@gmail.com>
2024-07-09 02:24:42 +0000

    Deploying to gh-pages from @ shuoshubao/shuoshubao.github.io@624a1a7d72bab51b949c533b1a6f511a289f6d4c 🚀

---
+++ assets/plaintext-7945c0c0.js
@@ -0,0 +1,1 @@
+function e(t){return{name:"Plain text",aliases:["text","txt"],disableAutodetect:!0}}export{e as default};
---
+++ assets/python-repl-a098ea60.js
@@ -0,0 +1,1 @@
+function n(a){return{aliases:["pycon"],contains:[{className:"meta.prompt",starts:{end:/ |$/,starts:{end:"$",subLanguage:"python"}},variants:[{begin:/^>>>(?=[ ]|$)/},{begin:/^\.\.\.(?=[ ]|$)/}]}]}}export{n as default};
