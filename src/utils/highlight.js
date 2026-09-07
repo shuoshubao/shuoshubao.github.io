d689f6d0d708de7592e9255ea029b868de5141ea
shuoshubao <shuoshubao@gmail.com>
2026-09-07 19:57:44 +0800

    feat: @highlightjs/cdn-assets

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -3,7 +3,7 @@ let hljsPromise = null;
 // 加载 highlight.js
 export const loadHljs = () => {
     hljsPromise ??= (async () => {
-        const { default: hljs } = await import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/core.js');
+        const { default: hljs } = await import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/core.min.js');
 
         const [
             { default: javascript },
@@ -17,16 +17,16 @@ export const loadHljs = () => {
             { default: yaml },
             { default: python }
         ] = await Promise.all([
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/javascript.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/xml.js'), // html 高亮走 xml 语法定义
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/markdown.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/bash.js'), // sh 复用 bash 语法定义
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/css.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/less.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/json.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/php.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/yaml.js'),
-            import('https://code.bdstatic.com/npm/highlight.js@11.12.0/es/languages/python.js')
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/javascript.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/xml.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/markdown.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/bash.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/css.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/less.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/json.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/php.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/yaml.min.js'),
+            import('https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/es/languages/python.min.js')
         ]);
 
         hljs.registerLanguage('javascript', javascript);
@@ -57,7 +57,7 @@ export const loadHljsCss = () => {
     hljsCssPromise ??= (() => {
         const link = document.createElement('link');
         link.rel = 'stylesheet';
-        link.href = 'https://code.bdstatic.com/npm/highlight.js@11.12.0/styles/vs2015.css';
+        link.href = 'https://code.bdstatic.com/npm/@highlightjs/cdn-assets@11.12.0/styles/vs2015.min.css';
         document.head.appendChild(link);
     })();
     return hljsCssPromise;
